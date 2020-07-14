69e3123c6ef992b912726be8d50409f87c4a814f
madalina <mada_cale@yahoo.com>
2020-07-14 19:57:23 +0300

    updated

--- Part 4.py
+++ Part 4.py
@@ -1,14 +1,15 @@
 import random
 
-#numbers which are divisible by 7 and multiple of 5 - ex1
+#ex 1 - numbers which are divisible by 7 and multiple of 5
+print('Ex 1')
 
-num_list = []
 for x in range(1500, 2701):
     if(x%7 == 0 and x%5 == 0):
-        num_list.append(str(x))
-print(num_list)
+        print(x)
 
-#guess number - ex 2
+# ex 2 guess a number between 1 to 9.
+#din documt de cautat chiar daca introduc string sa nu imi dea eroare
+print('Ex 2')
 
 number = random.randint(1,9)
 guess = int(input("Enter a number : "))
@@ -20,12 +21,18 @@ else:
     print("Well guessed !! " )
 
 
-#ex 3
+#ex 3 - print messages on the screen as long as a condition is met.
+print('Ex 3')
 
-val = 3
-val_user = int(input("Enter 3 value :"))
+value = input("Please enter 'exit' or 'continue' : ")
 
-if val == val_user:
-    print("Hello !!")
-else:
-    print(" ")
+while value == "continue":
+    message = input("Enter a message:")
+    print(message)
+if not value:
+    print(input("Please chose one option -> enter 'exit' or 'continue' : "))
+if value == "exit":
+    print(input("Enter a message:"))
+
+
+
