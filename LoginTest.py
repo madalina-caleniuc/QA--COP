f08b8f60875d42e8f84491d5db01f20742e99f50
madalina-caleniuc <50555000+madalina-caleniuc@users.noreply.github.com>
2020-08-04 16:56:40 +0300

    Add files via upload

---
+++ LoginTest.py
@@ -0,0 +1,23 @@
+
+from selenium import webdriver
+import time
+
+class LoginTest():
+
+    def setUpDriver(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    def login_validation(self):
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+        time.sleep(10)
+        self.driver.find_element_by_link_text("Welcome Admin").click()
+        self.driver.find_element_by_link_text("Logout").click()
+
+login1 = LoginTest()
+login1.setUpDriver()
+login1.login_validation()
